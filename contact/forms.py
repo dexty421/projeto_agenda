9b91f0dc7b5a84bb32154efb5b60e37bd9d0db60
Rafael <rafaelnaruto421@gmail.com>
2025-02-26 13:11:49 -0300

    Desenvolvendo para adicionar imagens no contatos

--- contact/forms.py
+++ contact/forms.py
@@ -8,11 +8,16 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
-    
+    picture = forms.ImageField(widget=forms.FileInput(
+        attrs={
+            'accept':'image/*'
+            }
+        )
+    )
     class Meta:
         model = models.Contact
         fields = (
-            'first_name','last_name','phone','email','description','category'
+            'first_name','last_name','phone','email','description','category','picture',
             )
         widgets = {
             'first_name':forms.TextInput(
